1b412fb044309de9d6c95c030304af3852d2c055
Kd526 <davisthoyah@gmail.com>
2024-02-15 13:37:34 +0300

    added dependencies

--- contracts/saleAgreement.sol
+++ contracts/saleAgreement.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ItemSale is ReentrancyGuard, Ownable {
+contract SaleAgreement is ReentrancyGuard, Ownable {
     // Struct to hold agreement details
     struct Agreement {
         address seller;
